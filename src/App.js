8b5de92af4573ce1c1000ee1059ed25006fb115a
Marek <repinski@hotmail.com>
2021-04-25 10:46:47 +0200

    Splashscreen page animatopn done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Nav from './components/Nav'
+import SplashScreen from './components/SplashScreen'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+      <div className="left-side"></div>
+      <Nav />
+      <SplashScreen />
+      <div className="right-side"></div>
     </div>
   );
 }
